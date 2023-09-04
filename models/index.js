605614accd4e9585f2e26c3eea0efe04ee039798
dylanadams98 <129101192+dylanadams98@users.noreply.github.com>
2023-09-04 20:19:50 +0100

    models

--- models/index.js
+++ models/index.js
@@ -3,14 +3,62 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const Model = require('sequelize');
+const Sequelize = require('../config/connection');
+
 
 // Products belongsTo Category
+class Product extends Model { }
+
 
 // Categories have many Products
 
-// Products belongToMany Tags (through ProductTag)
+Product.init(
+  {
+    product_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
+  },
+  {
+    Sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  }
+);
 
-// Tags belongToMany Products (through ProductTag)
+
 
 module.exports = {
   Product,
